d85cd2247931437db486f8773efeb2ed1efa363c
lanmower <almagestfraternite@gmail.com>
2023-09-29 11:56:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const crypto = require( 'hypercore-crypto');
 const b4a = require('b4a');
 const ipc = require('hyper-ipc-secure');
 const node = ipc();
+const discord = require('discord.js');
+
+const init = (kp, node=ipc(), serverKey=node.getSub(kp, process.env.SERVERNAME), callKey=node.getSub(kp, process.env.IPCNAME))=>{
 
 global.kp = crypto.keyPair(crypto.data(b4a.from('seedy')));
 
 require('dotenv').config(); //initialize dotenv
-const discord = require('discord.js');
 console.log({ discord });
 const client = new discord.Client({intents: [discord.GatewayIntentBits.Guilds,discord. GatewayIntentBits.GuildMessages, discord.GatewayIntentBits.GuildMessageReactions] }); //create new client
 client.on('ready', () => {
@@ -38,15 +40,19 @@ function splitAndCombine(inputString) {
 }
 
 //node.lbserve(parsed, 'testy', inp=>{console.log(inp); return inp});
-node.lbserve(node.getSub(kp, 'discord'), node.getSub(kp, 'serverbla'), 'discord', (inp)=>{
+node.lbserve(callKey, serverKey,process.env.IPCNAME, (inp)=>{
   const {channel, message} = inp;
   const chunks = splitAndCombine(message);
   chunks.forEach(message=>{
     ( client.channels.cache.get(channel) ).send(message)
   })
   return inp;
-})
+});
 
 console.log(process.env.DISCORD_TOKEN)
 //make sure this line is the last line
 client.login(process.env.DISCORD_TOKEN); //login bot using token
+  
+    return node;
+}
+
